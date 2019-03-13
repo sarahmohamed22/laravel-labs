5a2c6b114fb60a46e92eb907831ab5facc13befc
sarahmohamed22 <sarahmohamed@alexu.edu.eg>
2019-03-13 15:46:23 +0200

    the first laravel commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+  <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Title</th>
+        <th scope="col">Description</th>
+        <th scope="col">Creator Name</th>
+        <th class="col">Action</th>
+        </tr>
+    </thead>
+  </table>    
+@endsection
